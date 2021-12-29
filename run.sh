dba293e264df3c5971f169def9109bef9f6d0c82
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-12-29 17:35:08 +1300

    feat: Improve formatting

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ moonc -t out/ *.moon Entity/*.moon
 cd out
 
 # Merge into a single file
-lua-distill --input main.lua
+lua-distill --input main.lua > /dev/null
+echo $?
 
 cd ../embed
 
@@ -17,3 +18,4 @@ echo "var YouML = \`" >> YouML.js
 cat ../out/out.min.lua >> YouML.js
 echo "\`" >> YouML.js
 sed -i 's/\\/\\\\/g' YouML.js
+sed -i ':a;N;$!ba;s/\n/\\n/g' YouML.js
